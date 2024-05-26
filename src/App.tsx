5afaea032ef518991907d9957e91716506731947
Shaurya Gupta <shaurya50211@gmail.com>
2024-05-25 22:41:57 -0400

    playing around with props and state

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import ListGroup from "./components/ListGroup";
 
 function App() {
-  return <div><ListGroup /></div>
+  const items = [
+    "New York",
+    "San Francisco",
+    "Delhi",
+    "Tokyo",
+    "London",
+    "Paris",
+  ];
+
+  return <div><ListGroup items={items} heading="Cities" onSelectItem={(item: string) => console.log(item)}/></div>
 }
 
 export default App;
